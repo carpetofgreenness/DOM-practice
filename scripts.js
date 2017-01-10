fd3ce35f1294ae3d175582c30efc2c237eb3d5c0
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-01-10 11:34:53 -0500

    did exercises for slideshow 1

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-document.getElementById('main-section').innerHTML = "<h3>Hello World Smaller</h3>"
+function change(){
+  document.getElementById('el').innerHTML = "NEW TEXT";
+}
+
+var clementine = 5;
+var parsimmon = 10;
+console.log(clementine+parsimmon);
+
+var firstPart = "Hakunna";
+var lastPart = "Matatta";
+alert(firstPart + " " + lastPart);
+
+var me = {firstName: "Hannah", lastName: "Weinstein", birthday: "Nov. 27, 1990", interests: ["crochet","cooking","clarinet"]};
+console.log(me);
+console.log(me.interests);
