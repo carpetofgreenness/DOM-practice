aac1640fcf1fbcf48ea98e85f94a89fa134bdb8d
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-01-10 14:10:12 -0500

    day 2 excersies slideshow 2 - click

--- scripts.js
+++ scripts.js
@@ -6,3 +6,8 @@ heading2[0].innerText="here is a new title"
 for (i=0;i<heading2.length;i++) {
 	heading2[i].innerText = "This is article number "+i;
 }
+
+document.
+getElementById("clickOnMe").addEventListener("click", function(event){
+  alert(document.getElementById("clickOnMe").innerText);
+})
