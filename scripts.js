193d9a40aaaaee7f16033cca71222a1d546867ff
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-01-10 11:46:06 -0500

    did exercises for slideshow 2, deleted slideshow 1

--- scripts.js
+++ scripts.js
@@ -1,15 +1,3 @@
-function change(){
-  document.getElementById('el').innerHTML = "NEW TEXT";
-}
-
-var clementine = 5;
-var parsimmon = 10;
-console.log(clementine+parsimmon);
-
-var firstPart = "Hakunna";
-var lastPart = "Matatta";
-alert(firstPart + " " + lastPart);
-
-var me = {firstName: "Hannah", lastName: "Weinstein", birthday: "Nov. 27, 1990", interests: ["crochet","cooking","clarinet"]};
-console.log(me);
-console.log(me.interests);
+console.log("hello world")
+debugger;
+alert('I think I found the bug!');
